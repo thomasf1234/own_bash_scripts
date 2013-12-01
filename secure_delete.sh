c0c5c4a9c7f3d8d84167c57e84f14008be6551e1
ad <ad@ad-deu64.(none)>
2013-12-01 18:33:49 +0000

    structured secure_delete.sh better

--- secure_delete.sh
+++ secure_delete.sh
@@ -4,6 +4,27 @@
 #must pass in the file path of the non-temporary file i.e.
 #bash secure_delete.sh /home/username/dir/file
 
+#need to fix
+function check_input {
+  if [ "$#" -eq 0 ];
+  then
+    echo "no arguments provided, this method accepts 1 argument; a file_path i.e '/home/username/dir/file'"
+  elif [ "$#" -gt 1 ];
+  then
+    echo "Too many arguments, this method accepts 1 argument; a file_path i.e '/home/username/dir/file'"
+  else
+    securely_delete_file_with_temporary $1 
+  fi
+}
+
+
+function securely_delete_file_with_temporary {
+secure_delete_file $1
+delete_temp_file_also $1
+check_if_still_there $1
+}
+
+
 function secure_delete_file {
 
   if [ ! -f "$1" ];
@@ -16,18 +37,13 @@ function secure_delete_file {
   FILE_PATH=$1 #"/home/ad/insert_command.psql"
   DIR=${FILE_PATH%/*}
   FILE_NAME=${FILE_PATH##*/} #$(basename $FILE_PATH)
-  #echo $FILE_PATH
-  #echo $DIR
-  #echo $FILE_NAME
   BYTES="$(cd $DIR && (ls -lt | grep $FILE_NAME$ | awk '{print $5}'))"
-  #echo $BYTES
   COMMAND="dd if=/dev/urandom of=$FILE_PATH bs=$BYTES count=1 conv=notrunc"
   echo "**********WIPING '$FILE_PATH' **********"
   for ((n=1;n<8;n++))
   do 
     echo "beginning to execute secure wipe $n ..."
     $COMMAND 
-    #echo "$COMMAND"
     EXIT_STATUS="$?"
     echo "finished executing secure wipe $n , exit status: $EXIT_STATUS."
     echo ""
@@ -51,16 +67,8 @@ function delete_files {
   fi
 }
 
-function check_if_still_there {
-  if [ -f "$1" ] || [ -f "$1~" ]
-  then
-    echo "*******DANGER!! FILE IS STILL THERE, YOU MUST INVESTIGATE, MAY NOT BE WIPED!!!!*******"
-    #securely_delete_file_with_temporary
-  else
-    echo "**********files have been securely deleted.**************"
-  fi
-}
 
+#need to fix
 function check_if_there {
   if [ -f "$1" ];
   then
@@ -72,24 +80,16 @@ function check_if_there {
 }
 
 
-function securely_delete_file_with_temporary {
-secure_delete_file $1
-delete_temp_file_also $1
-check_if_still_there $1
-}
-
-#need to fix
-function check_input {
-  if [ "$#" -eq 0 ];
+function check_if_still_there {
+  if [ -f "$1" ] || [ -f "$1~" ];
   then
-    echo "no arguments provided, this method accepts 1 argument; a file_path i.e '/home/username/dir/file'"
-  elif [ "$#" -gt 1 ];
-  then
-    echo "Too many arguments, this method accepts 1 argument; a file_path i.e '/home/username/dir/file'"
+    echo "*******DANGER!! FILE IS STILL THERE, YOU MUST INVESTIGATE, MAY NOT BE WIPED!!!!*******"
+    #securely_delete_file_with_temporary
   else
-    securely_delete_file_with_temporary $1 
+    echo "**********files have been securely deleted.**************"
   fi
 }
+
 
 check_input $1
 
