2efe0dbaa717ec8279d3f62aebbd770bf4534aea
fpdev <fpdev@gouda.(none)>
2014-02-11 17:57:49 +0000

    temporarily added formmatter.py, should be removed

---
+++ formatter.py
@@ -0,0 +1,11 @@
+import subprocess
+
+class Formatter:
+   #raw_data = {'service1': 'results1, 'service2': 'results2', 'service3': 'results3'}
+   def __init__(self, raw_data):
+       self.raw_data = raw_data
+   
+   def 
+   result = subprocess.check_output('grep --color -ir service *', shell=True) #try to bypass this by filtering the directories to search before hand
+   print result
+        
